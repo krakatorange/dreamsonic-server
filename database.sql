891d19584c5167d7b197eec2c8353def962f682d
Misha <mikhail.aleksandrov@nvc-inc.net>
2023-04-14 02:27:26 -0400

    added share route

--- database.sql
+++ database.sql
@@ -1,6 +1,14 @@
 //CREATE DATABASE antisonnik;
 
-CREATE TABLE users(
-  user_id SERIAL PRIMARY KEY,
-  description VARCHAR(255)
+CREATE TABLE dreams(
+  doc_id SERIAL,
+  user_id character varying(255) NOT NULL,
+  dream_id character varying(255) NOT NULL,
+  role character varying(50),
+  gender character varying(20),
+  content character varying,
+  content_eng character varying,
+  dream character varying,
+  created_at timestamp without time zone NOT NULL DEFAULT now(),
+  CONSTRAINT dreams_pkey PRIMARY KEY (doc_id)
 );
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require("cors");
 const pool = require("./db");
 const axios = require("axios");
+const { v4: uuidv4 } = require('uuid');
 const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 const dotenv = require('dotenv').config();
 
@@ -18,86 +19,15 @@ app.use(expressCspHeader({
 
 //ROUTES//
 
-//create a todo
-
-app.post("/todos", async (req, res) => {
-  try {
-    const { description } = req.body;
-    const newTodo = await pool.query(
-      "INSERT INTO users (description) VALUES($1) RETURNING *",
-      [description]
-    );
-
-    res.json(newTodo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-//get all todos
-
-app.get("/todos", async (req, res) => {
-  try {
-    const allTodos = await pool.query("SELECT * FROM users");
-    res.json(allTodos.rows);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-//get a todo
-
-app.get("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const todo = await pool.query("SELECT * FROM users WHERE user_id = $1", [
-      id
-    ]);
-
-    res.json(todo.rows[0]);
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-//update a todo
-
-app.put("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { description } = req.body;
-    const updateTodo = await pool.query(
-      "UPDATE users SET description = $1 WHERE user_id = $2",
-      [description, id]
-    );
-
-    res.json("Todo was updated!");
-  } catch (err) {
-    console.error(err.message);
-  }
-});
-
-//delete a todo
-
-app.delete("/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deleteTodo = await pool.query("DELETE FROM users WHERE user_id = $1", [
-      id
-    ]);
-    res.json("Todo was deleted!");
-  } catch (err) {
-    console.log(err.message);
-  }
-});
-
 // create a dream
-
 app.post("/dream", async (req, res) => {
   try {
     const { user_id, role, gender, content } = req.body;
+    const dream_id = uuidv4();
     const endpoint = process.env.AWS_APIGATEWAY_LAMBDA_CHATGPT_ENDPOINT;
     const api_key = process.env.AWS_APIGATEWAY_LAMBDA_CHATGPT_API_KEY;
+
+    console.log(req.body);
 
     const lambda_res = await axios.post(endpoint, {
                         user_id: user_id,
@@ -114,11 +44,11 @@ app.post("/dream", async (req, res) => {
                         console.log(error)
                       })
     
-    console.log(lambda_res['data']['body']['content'])
+    console.log(lambda_res)
 
     const newTodo = await pool.query(
-      "INSERT INTO dreams (user_id, role, gender, content, dream) VALUES ($1,$2,$3,$4,$5) RETURNING *",
-      [user_id, role, gender, lambda_res['data']['body']['content'], content]
+      "INSERT INTO dreams (user_id, dream_id, role, gender, content, content_eng, dream) VALUES ($1,$2,$3,$4,$5,$6,$7) RETURNING *",
+      [user_id, dream_id, role, gender, lambda_res['data']['body']['content'], lambda_res['data']['body']['content_eng'], content]
     );
                       
     res.json(lambda_res['data']);
@@ -128,11 +58,24 @@ app.post("/dream", async (req, res) => {
 });
 
 // get a dream
-
 app.get("/dream/:id", async (req, res) => {
   try {
     const { id } = req.params;
     const dream = await pool.query("SELECT * FROM dreams WHERE user_id = $1 ORDER BY created_at DESC LIMIT 1", [
+      id
+    ]);
+
+    res.json(dream.rows[0]);
+  } catch (err) {
+    console.error(err.message);
+  }
+});
+
+// get a shared dream
+app.get("/share_dream/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const dream = await pool.query("SELECT * FROM dreams WHERE dream_id = $1", [
       id
     ]);
 
